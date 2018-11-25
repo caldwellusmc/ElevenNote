c01d8c10ab6042d88b8c76fc3dca73e95ce75180
caldwellusmc <deshanecaldwell@yahoo.com>
2018-11-25 15:12:54 -0500

    fourth commit

---
+++ ElevenNote.Models/NoteListItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    public class NoteListItem
+    {
+        public int NoteId { get; set; }
+        public string Title { get; set; }
+
+        [Display(Name="created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public override string ToString() => Title;
+        {
+            return base.ToString();
+        }
+    }
+}
